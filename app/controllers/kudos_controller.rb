ac405e01ee5ede2d830d3d3bd3a2fc37f316d202
Wojciech Balon <balonw@gmail.com>
2023-01-24 16:35:43 +0100

    Auto stash before cherry pick of "set root route, setup devise gem, added a navbar feature,added home button, added seeding"

--- app/controllers/kudos_controller.rb
+++ app/controllers/kudos_controller.rb
@@ -1,4 +1,9 @@
 class KudosController < ApplicationController
+
+  def show
+    @kudo = Kudo.find(params[:id])
+  end
+
   def index
     @kudos = Kudo.all
   end
@@ -18,6 +23,21 @@ class KudosController < ApplicationController
     end
   end
 
+  def update
+    @kudo = Kudo.find(params[:id])
+    if @kudo.update(kudo_params)
+      flash[:notice] = 'Kudo was edited successfully'
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+      @kudo = Kudo.find(params[:id])
+      @kudo.destroy
+      redirect_to root_path
+  end
   private
 
   def kudo_params
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,9 +3,14 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+<<<<<<< Updated upstream
 <<<<<<< HEAD
   has_many :given_kudos, class_name: 'Kudo', foreign_key: 'giver_id'
   has_many :received_kudos, class_name: 'Kudo', foreign_key: 'receiver_id'
 =======
 >>>>>>> c2b5774... set root route, setup devise gem, added a navbar feature,added home button, added seeding
+=======
+  has_many :given_kudos, class_name: 'Kudo', foreign_key: 'giver_id'#, dependent: :kudo
+  has_many :received_kudos, class_name: 'Kudo', foreign_key: 'receiver_id'#, dependent: :kudo
+>>>>>>> Stashed changes
 end
