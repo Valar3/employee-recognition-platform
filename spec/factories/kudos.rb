27807930d887d54fc336063203ff93c234437df4
Wojciech Balon <balonw@gmail.com>
2023-01-31 10:44:54 +0100

    stash

--- spec/factories/kudos.rb
+++ spec/factories/kudos.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
      title {'my first factory kudo'}
      content {'this is my first kudo created by a factory'}
      giver_id {current_employee_id}
-     receiver_id { '6' }
+     receiver_id { '1' }
   end
 end
