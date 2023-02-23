902324753bc01776e425e407e5879dae887a6946
Wojciech Balon <balonw@gmail.com>
2023-02-23 20:57:03 +0100

    Transaction - final fix

--- app/controllers/kudos_controller.rb
+++ app/controllers/kudos_controller.rb
@@ -21,15 +21,12 @@ class KudosController < EmployeesController
     @kudo = Kudo.new(kudo_params)
     @kudo.giver_id = current_employee.id
     Employee.transaction do
-      if @kudo.save!
+        @kudo.save!
         @current_employee.number_of_available_kudos -= 1
         @current_employee.save!
 
         flash[:notice] = 'Kudo was created successfully'
         redirect_to root_path
-      else
-        render 'new'
-      end
     rescue ActiveRecord::RecordInvalid
       render 'new'
     end
