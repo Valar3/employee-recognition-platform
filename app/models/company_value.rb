29df20f11bb8894f57d5e6db27b5a987b34a2053
Wojciech Balon <balonw@gmail.com>
2023-02-24 14:33:17 +0100

    final fix

--- app/models/company_value.rb
+++ app/models/company_value.rb
@@ -1,4 +1,6 @@
 class CompanyValue < ApplicationRecord
   validates :title, presence: true
   validates :title, uniqueness: { on: :create, message: 'has already been taken' }
+  validates :title, uniqueness: { on: :create, message: 'has already been taken' }
+
 end
