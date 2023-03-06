e5d6dce04df497f5e0f75602a386ead3cdeacdcb
Wojciech Balon <balonw@gmail.com>
2023-03-06 13:00:56 +0100

    Sprint-3-task-3
    Fixed all the CR comments

--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Reward, type: :model do
     create(:reward)
   end
 
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_presence_of(:price) }
   it { is_expected.to validate_numericality_of(:price), message: 'is not a number' }
+  it { is_expected.to validate_numericality_of(:price), greater_than_or_equal_to: 1, message: 'is a valid numer' }
+  it { is_expected.to validate_numericality_of(:price), lesser_than: 1, message: 'is not a valid numer' }
 end
