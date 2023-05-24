6f01e8c90f4a611e4e4675fb8974bc81e5374fec
Wojciech Balon <balonw@gmail.com>
2023-05-24 14:02:15 +0200

    working commit #2

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -3,7 +3,6 @@ class Reward < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 1 }
-  has_many :reward_categories
-  has_many :catergories, through: :reward_categories
-
+  has_one :reward_category
+  has_one :catergory, through: :reward_categories
 end
