477639cdb4734c96c030040279fdfc59c2065e41
Wojciech Balon <balonw@gmail.com>
2023-07-04 12:40:59 +0200

    Mid Sprint-7-task-3
    having trouble with category_id

--- app/controllers/admins/pages_controller.rb
+++ app/controllers/admins/pages_controller.rb
@@ -1,5 +1,6 @@
 module Admins
   class PagesController < AdminController
     def dashboard; end
+    def import; end
   end
 end
--- app/controllers/admins/rewards_controller.rb
+++ app/controllers/admins/rewards_controller.rb
@@ -42,6 +42,15 @@ module Admins
       end
     end
 
+    def import
+      return redirect_to request.referer, notice: 'No file added' if params[:file].nil?
+      return redirect_to request.referer, notice: 'Only CSV files allowed' unless params[:file].content_type == 'text/csv'
+
+      ImportCsvService.new.call(params[:file])
+
+      redirect_to request.referer, notice: 'Import started...'
+    end
+
     private
 
     def reward_params
