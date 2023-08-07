8318de0d09f8394ed3b0164cb0d83ce03812564b
Wojciech Balon <balonw@gmail.com>
2023-08-08 01:03:33 +0200

    Rubocop and rspec fix

--- app/services/import_csv_service.rb
+++ app/services/import_csv_service.rb
@@ -4,13 +4,12 @@ class ImportCsvService
     opened_file = File.open(file)
     options = { headers: true, col_sep: ';' }
     CSV.foreach(opened_file, **options) do |row|
-    current_row = row
-            reward = Reward.find_or_initialize_by(title: row['Title'])
-            reward.description = row['Description']
-            reward.price = row['Price']
-            reward.category = Category.find_or_create_by!(title: row['Category'])
-            reward.delivery_method = row['Delivery method']
-            reward.save!
+      reward = Reward.find_or_initialize_by(title: row['Title'])
+      reward.description = row['Description']
+      reward.price = row['Price']
+      reward.category = Category.find_or_create_by!(title: row['Category'])
+      reward.delivery_method = row['Delivery method']
+      reward.save!
     end
   end
 end
